f68c88399ed2c5fcdaa9f19d9f6ed1386be79a12
gosfi <samLef12@outlook.com>
2021-01-29 20:22:30 -0500

    Update Win32_handmade.cpp

--- code/Win32_handmade.cpp
+++ code/Win32_handmade.cpp
@@ -7,6 +7,16 @@ int WINAPI WinMain
   LPSTR lpCmdLine,
   int nShowCms)
 {
+  WNDCLASS windowClass ={};
+
+  //TODO(sam)check if style is still relevant
+  windowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+  windowClass.lpfnWndProc = ;
+  windowClass.hInstance = hInstance;
+  // HICON     hIcon;
+  // LPCSTR    lpszMenuName;
+  windowClass.lpszClassName = "handMadeHeroWindowClass";
+  
   MessageBox(0,"this is a window"," handmade hero",
 	     MB_OK|MB_ICONINFORMATION);
   return(0);
