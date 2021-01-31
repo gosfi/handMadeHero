5d6b3a4ccf63d6cef13db73d511ab753a75e146b
gosfi <samLef12@outlook.com>
2021-01-31 18:58:36 -0500

    allocating backbuffer

--- code/Win32_handmade.cpp
+++ code/Win32_handmade.cpp
@@ -8,29 +8,37 @@ global_variable bool Running;
 global_variable BITMAPINFO BitmapInfo;
 global_variable void *BitmapMemory;
 global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
 
 internal void
 Win32ResizeDIBSection(int Width, int Height)
 {
   
-  if(BitmapInfo.bmiHeader.biSize)
+  if(BitmapHandle)
   {
-    
+    DeleteObject(BitmapHandle);
+  }
+  else
+  {
+    BitmapDeviceContext = CreateCompatibleDC(0);
   }
   
   BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
-  BitmapInfo.biWidth = Width;
-  BitmapInfo.biHeight = Height;
-  BitmapInfo.biPlanes = 1;
-  BitmapInfo.biBitCount = 32;
-  BitmapInfo.biCompression = BI_RGB;
+  BitmapInfo.bmiHeader.biWidth = Width;
+  BitmapInfo.bmiHeader.biHeight = Height;
+  BitmapInfo.bmiHeader.biPlanes = 1;
+  BitmapInfo.bmiHeader.biBitCount = 32;
+  BitmapInfo.bmiHeader.biCompression = BI_RGB;
   
+ 
   BitmapHandle =  CreateDIBSection(
-				   DeviceContext,
+				   BitmapDeviceContext,
 				   &BitmapInfo,
 				   DIB_RGB_COLORS,
 				   &BitmapMemory,
 				   0,0);
+
+  ReleaseDC(0,BitmapDeviceContext);
 }
 
 internal void
@@ -39,8 +47,8 @@ Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
   StretchDIBits(DeviceContext,
 		X,Y,Width,Height,
 		X,Y,Width,Height,
-                const VOID *lpBits,
-                const BITMAPINFO *lpbmi,
+                BitmapMemory,
+                &BitmapInfo,
                 DIB_RGB_COLORS,SRCCOPY);
 }
 
